b1d178157f387ed661a8aefe901b6569ee93da5e
Robin Houdmeyers <houdmeyers@gmail.com>
2016-01-24 21:18:43 +0100

    initial commit

---
+++ NOTES.md
@@ -0,0 +1,12 @@
+# Nice POSTCSS wins
+- https://github.com/postcss/postcss-bem-linter
+- postcss-font-magician
+- https://github.com/SlexAxton/css-colorguard
+- https://github.com/btholt/postcss-colorblind
+
+# Tryouts
+- http://cssnano.co
+- stylelint
+- https://github.com/peterramsing/lost
+
+- vendor prefixes are handled by 'autoprefixer'
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var webpack           = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+var autoprefixer = require('autoprefixer');
+var precss       = require('precss');
+var fontMagician = require('postcss-font-magician');
+var atImport     = require('postcss-import');
+var bemLinter    = require('postcss-bem-linter');
+var formatter    = require('postcss-reporter/lib/formatter');
+
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader!postcss-loader"
+      }
+    ],
+  },
+  postcss: function(webpack) {
+    return [
+      atImport({
+        // addDependencyTo: webpack
+      }),
+      // precss,
+      // autoprefixer({ browsers: ['last 2 versions'] }),
+      // fontMagician,
+      bemLinter('bem'),
+    ];
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      template: 'src/index.html', // Load a custom template
+      inject: 'body' // Inject all scripts into the body
+    })
+  ],
+  devtool: 'source-map',
+}
